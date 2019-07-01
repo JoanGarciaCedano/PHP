b3f377ab0a18dab0ad63f07df7b489f986a6bb85
JoanGarciaCedano <joan.gc.mail@gmail.com>
2019-07-01 16:21:37 -0600

    Hola Mundo y Variables en PHP

---
+++ 01 - Hola Mundo PHP/hola_mundo.php
@@ -0,0 +1,17 @@
+<?php 
+
+	//echo nos sirve para mostrar información en pantalla.
+	//echo "Hola Mundo";
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Documento</title>
+ </head>
+ <body>
+ 	<h1><?php echo "Hola Mundo"; ?></h1>
+ </body>
+ </html>
---
+++ 02 - Variables/variables.php
@@ -0,0 +1,17 @@
+<?php 
+
+//String : Cadena de texto
+$nombre = 'Joan';
+//Integer : Enteros
+$numero = 8;
+//Double : Decimales
+$numero_decimal = 8.8;
+//Boolean : Booleandos (true | false)
+$verdadero = false;
+
+echo "Hola $nombre". " " . $numero. " " . $numero_decimal. " + " .$verdadero;
+echo $verdadero;
+
+echo gettype($verdadero);
+
+ ?>
